a3f6b0e17a8e72f3123db0d012ca55a751eba1da
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-07-25 19:54:06 -0500

    more examples for students

--- index.js
+++ index.js
@@ -36,3 +36,90 @@ for (let index = 0; index < myHand.length; index++){
         console.log(`card ${myHand[index]} is now the biggest number`)
     }
 }
+
+let myHandObjs = [
+    { number: 7,  suit: 'Clubs'},
+    { number: 6, suit: 'Spades'},
+    { number: 8, suit: 'Spades'},
+    { number: 9, suit: 'Hearts'},
+    { number: 2, suit: 'Hearts'},
+]
+//get the first card
+let highestCard = myHandObjs[0]
+//initializer; condition; iterator
+for (let index = 0; index < myHandObjs.length; index++){
+    //for loop body is stuff in {}
+    console.log( `at myHandObjs[${index}] the card is:`, myHandObjs[index])
+    //if the number that I'm at is bigger keep it in highest number variable
+    console.log("comparing", myHandObjs[index],highestCard)
+    //to compare the cards what to do?
+    //lets look at the number property on each
+    //console.log(myHandObjs[index].Number) //if property is mis-capitalized or mispelled that property is undefined
+    if (myHandObjs[index].Number > highestCard.number){
+        highestCard = myHandObjs[index]
+        console.log(myHandObjs[index], "is now the biggest card")
+    }
+}
+
+let groceryObjs = [
+    {name: "tomatoes",price: 2.99 },
+    {name: "potatoes",price:  1.37 },
+    {name: "pasta", price: .99},
+]
+console.log("groceries",groceryObjs)
+//calcuate the total cost of groceries
+
+let totalCost = 0
+//go through the length of groceries
+    //each item add grocery item's price to totalCost
+for (let index = 0; index < groceryObjs.length; index++) {
+    const groceryItem = groceryObjs[index];
+    console.log("groceryItem", groceryItem)
+    totalCost = totalCost + groceryItem.price
+}
+
+// alert(`Jonathon please pay ${totalCost}`)
+
+let tickets = [
+    {
+        airline: "delta",
+        class: "first-class",
+        price: 74
+    },
+    {
+        airline: "delta",
+        class: "economy",
+        price: 65
+    },
+    {
+        airline: "delta",
+        class: "coach",
+        price: 54
+    }
+]
+
+let airlineExpenses = [
+    {
+        item: "fuel",
+        cost: 7000
+    },
+    {
+        item: "crew-salary",
+        cost: 2000
+    }
+]
+
+//calculate the profits of these tickets
+let totalTicketProfits = 0
+for (let index = 0; index < tickets.length; index++) {
+    const currentTicket = tickets[index];
+    totalTicketProfits = totalTicketProfits + currentTicket.price
+}
+console.log("totalTicketProfits",totalTicketProfits)
+
+let totalExpenses = 0
+for (let index = 0; index < airlineExpenses.length; index++) {
+    const currentExpense = airlineExpenses[index];
+    totalExpenses = totalExpenses + currentExpense.cost
+}
+console.log(totalTicketProfits - totalExpenses)
